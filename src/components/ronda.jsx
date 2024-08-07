1f45a57889c839635073360f41525e86b0085a39
dnxtpro <dnxtpro@gmail.com>
2024-08-07 23:02:21 +0100

    sexxxx

--- src/components/ronda.jsx
+++ src/components/ronda.jsx
@@ -8,8 +8,9 @@ function Ronda() {
   return (
       
 <div className='flex flex-col mx-auto'>
-    <div className=' perso flex flex-row ml-64 '>
-        <img src="/burro.PNG" className='w-1/4' alt="" />
+  <div><img src="/rondaz.PNG " className='w-2/4 mx-auto' alt="" /></div>
+    <div className=' perso flex flex-row mx-32 space-x-8  '>
+        <img src="/burro.PNG" className='w-3/12 h-5/6' alt="" />
     <h1 className='w-2/12 '>
     Berrad 
     
@@ -26,14 +27,29 @@ function Ronda() {
     
     
     </h1>
-  
-<img src="/berrad.jpg" className='ca' alt="" />    
-<img src="/berrad.GIF " className='w-1/4' alt="" />
+    <h1 className='w-2/12 '>
+    Berrad 
+
+“Ronda by Ibouyen” est une nouvelle réflexion et une réinterprétation du plus célèbre des jeux de cartes au Maroc, un jeu de cartes que l’on retrouve dans plusieurs pays de la Méditerranée, probablement transmis par les Tziganes à travers tout le Nord de la méditerranée puis dans le Maghreb. 
+
+Dans cette version, l’artiste remplace les coupes par des théières, les théières (Berrad en darija) nous rappellent des instants du passé, des moments passés en famille ou seul, intimité partagée, une douce nostalgie.
+
+Dans chacune d’elles, nous voyons des reflets et des formes différentes, rendant chaque théière unique, une sorte de miroir de l’intime, des moments figés comme sur des photos instantanées. 
+
+Parmi les quatres objets, celui-ci est le plus intime, se concentrant sur l’individu, il nous invite à observer notre propre reflet, un monde de formes onirique capté sur le métal de la théière, un peu comme pour les premières pellicules utilisant de l’argent pour figer la lumière. 
+
+Ce fut le premier objet, le début de ce projet, le commencement d’un voyage dans le monde des symboles.
+    
+    
+    
+    </h1>  
+<img src="/berrad.jpg" className=' w-1/12' alt="" />    
+<img src="/sota.PNG " className='w-1/4 h-5/6' alt="" />
    
     </div>
-    <div className=' perso flex flex-row'>
-    <img src="/burro.PNG" className='w-1/4' alt="" />
-    <h1 className='w-1/12'>The coins from the original card game are replaced by faces, the circular shape is repeated, but we discover faces (Kemmara) with distinct and diverse features.
+    <div className=' perso flex flex-row space-x-8 mx-32'>
+    <img src="/burro.PNG" className='w-3/12 h-5/6' alt="" />
+    <h1 className='w-2/12 mt-32'>The coins from the original card game are replaced by faces, the circular shape is repeated, but we discover faces (Kemmara) with distinct and diverse features.
     
     This object is inspired by the heterogeneity found in North Africa, we see different faces and different Amazigh tattoos which represent symbols of nature, mainly those related to the lunar.
     
@@ -41,19 +57,55 @@ function Ronda() {
     
     This time, the object transcends the individual and shows the community, the regional: a unity in diversity, a fight to maintain authenticity, even in a world which tends towards standardization.
     </h1>
-    <img className='ca' src="/kemara.jpg" alt="" />
+    <h1 className='w-2/12 mt-32'>Kemara : 
+
+
+Les pièces de monnaie, du jeu de cartes original sont remplacées par des visages, la forme circulaire se répète, mais nous y découvrons des visages (Kemmara) aux traits distincts et divers.
+
+Cet objet s’inspire de l’hétérogénéité que l’on retrouve en Afrique du nord, nous voyons différents visages et différents tatouages Amazighs qui représentent des symboles de la nature, principalement ceux qui ont attrait au lunaire.
+
+Curieusement, les visages dans le jeu de cartes nous rappellent des pleines lunes, mais pas que ça.
+
+Cette fois-ci, l’objet transcende l’individu et montre la communauté, le régional : une unité dans la diversité, un combat pour maintenir l’authenticité , même dans un monde qui tend vers l'uniformisation. </h1>
+    <img className='w-1/12' src="/kemara.jpg" alt="" />
+    <img className='w-3/12 h-5/6' src="/sota.PNG" alt="" />
     </div>
-  <div className='w-1/5 perso'> The “Nejma” (stars in Arabic) are the crosses or stars of Agadez, we can find them in jewelry and decorative designs of several ethnic groups in North Africa.
+
+
+    
+  <div className='perso flex flex-row space-x-8 mx-32'>
+  
+  <img src="/burro.PNG" className='w-3/12 h-5/6' alt="" />
+  <img className='w-1/12' src="/nejma.jpg" alt="" />
+  <h1 className='w-2/12 mt-32'>
+  The “Nejma” (stars in Arabic) are the crosses or stars of Agadez, we can find them in jewelry and decorative designs of several ethnic groups in North Africa.
     
     This fascinating symbol is the first star that appears in the sky, the star that guides the nomads in the desert, “the eye of God” as the “blue men” call it, symbol of eternal beginnings, it guides them towards truth and justice.
     
     The swords of the original game, which are tools of war, are replaced in the version of “Ibouyen” by a transcendental symbol and guidance.
     
     In Amazigh mythology, the first star that appears in the sky is the goddess “Taïa”, daughter of “Ayyour”: the moon, she is the engendered mother and the mother who will engender the other stars, following her appearance.
+    </h1> 
     
-    
+  <h1 className='w-2/12 mt-32'>
+  Nejma : 
+
+Les “Nejma” ( étoiles en arabe) sont les croix ou étoiles d’Agadez, on peut les retrouver dans les bijoux et les dessins décoratifs de plusieurs ethnies d’Afrique du nord.
+
+Ce symbole fascinant est la première étoile qui apparaît dans le ciel, l’étoile qui guide les nomades dans le désert, “l’oeil de Dieu” comme la désignent les “hommes bleus”, symbole de l’éternel recommencement, elle les guide vers la vérité et la justice.
+
+Les épées du jeu original, qui sont des outils de guerre, sont remplacées dans la version de “Ibouyen” par un symbole transcendantal et de guidance.
+
+Dans la mythologie Amazighe, la première étoile qui apparaît dans le ciel est la déesse “Taïa”, fille d’”Ayyour” : la lune, elle est la mère engendrée et la mère qui engendrera les autres étoiles, suivant son apparition. 
+  </h1> 
+  <img className='w-3/12 h-5/6' src="/sota.PNG" alt="" />
   </div>
-    <div className='w-1/5 perso'>  Finally, the sticks from the original game turn into statues. Statues that can be found in different cultures throughout the world and history. 
+    <div className=' perso flex flex-row space-x-8 mx-32'>  
+    <img src="/carass.PNG" className='w-3/12 h-5/6' alt="" />
+  
+  <img className='w-1/12' src="/bashar.jpg" alt="" />
+  <h1 className='w-2/12 mt-32'>
+  Finally, the sticks from the original game turn into statues. Statues that can be found in different cultures throughout the world and history. 
     
     They might seem disconnected, but the human prevails, because the human is the only living being to play cards, to play his life sometimes through choices.
     
@@ -61,7 +113,24 @@ function Ronda() {
     
     We move from the transcendental to the corporeal, to the tangible. We extend the regional to the global. We can appreciate how interest in the body is universal. The further we move away, the more we can appreciate what is near.
     
-    “The symbol is used to recognize oneself.”</div>
+    “The symbol is used to recognize oneself.”
+    </h1>
+  <h1 className='w-2/12 mt-32'>
+  Bashar :
+
+
+Enfin, les bâtons du jeu original se transforment en statues. Des statues que l'on peut trouver dans différentes cultures à travers le monde et l’histoire. 
+
+Elles pourraient sembler déconnectées, mais l'humain prévaut, car l’humain est le seul être vivant à jouer aux cartes, à jouer sa vie parfois à travers des choix.
+
+Pour les unifier dans ce jeu de cartes, la couleur primordiale, il y a quatre couleurs principales : le bleu, le vert, le marron et le violet. Les statues contiennent, dans une certaine mesure, toutes ces nuances.
+
+Nous passons du transcendantal au corporel, au tangible. Nous étendons le régional au global. Nous pouvons apprécier comment l'intérêt pour le corps est universel. Plus nous nous éloignons, plus nous pouvons apprécier ce qui est proche.
+
+"Le symbole sert à se reconnaître."
+    </h1>
+    <img className='w-3/12 h-5/6' src="/sota.PNG" alt="" />
+    </div>
 </div>
    
 
