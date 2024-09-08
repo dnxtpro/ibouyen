6e44b37a66e1c9d2866991124b28765bba43460b
dnxtpro <161753397+dnxtpro@users.noreply.github.com>
2024-09-08 14:49:43 +0200

    ultima pubs1

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -7,7 +7,7 @@ function Aboutme() {
      <div className="container px-5 py-6 mx-auto hei ">
         <div className="flex flex-col  h-auto justify-center content-center align-middle items-center">
                 <img src="/fadelpi.png" alt="" className=''/>
-                <h2 className='md:w-3/4  fontp' >I’m Ibouyen, a 21-year-old visual artist and art history student. I enjoy exploring different techniques in my work, from digital art to watercolor, markers, acrylics, oils, and even murals. I draw inspiration from the world of dreams, literature, and North African culture. I’m always looking to learn and grow with each piece I create, constantly searching for new ways to express myself and understand the world through art.</h2>
+                <h2 className='w-3/4  fontp' >I’m Ibouyen, a 21-year-old visual artist and art history student. I enjoy exploring different techniques in my work, from digital art to watercolor, markers, acrylics, oils, and even murals. I draw inspiration from the world of dreams, literature, and North African culture. I’m always looking to learn and grow with each piece I create, constantly searching for new ways to express myself and understand the world through art.</h2>
         </div>
 
      </div>
